9b00f0c4de0f89470209f2659b779a8b4f98f436
Arthur Tirado <arthurmegiolaro@gmail.com>
2024-03-01 11:48:56 -0500

    Create Vacances-DML-Select.sql

---
+++ Vacances-DML-Select.sql
@@ -0,0 +1,8 @@
+/* **********************************************************
+	DML Select
+	Schéma MRD:	"Cas Village Vacances"
+	Auteur:		Arthur Tirado
+***********************************************************/
+
+USE VILLAGE_VACANCES
+
