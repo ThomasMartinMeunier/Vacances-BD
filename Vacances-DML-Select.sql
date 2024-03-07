b3a0b708767821c2ac5b55cc8680eca678b64a2d
ThomasMartinMeunier <142697221+ThomasMartinMeunier@users.noreply.github.com>
2024-03-06 20:16:12 -0500

    Question 2

--- Vacances-DML-Select.sql
+++ Vacances-DML-Select.sql
@@ -45,6 +45,39 @@ D3                 Chalet 2 personnes                  3            tennis, pisc
 */
 
 /*
+--2
+Produire la liste des catégories de village et calculer le tarif moyen des nuitées pour chacune.
+• Pour chaque catégorie de village, indiquer dans l’ordre :
+- Le numéro de la catégorie du village,
+- La description de la catégorie du village,
+- Le prix moyen par personne et par nuit des logements avec le format d’affichage :
+63,75 $ (format monétaire canadien français, 2 chiffres après la virgule)
+• Trier par catégorie de village
 */
+
+SELECT
+	CATEGORIE_VILLAGE.NO_CATEGORIE,
+	CATEGORIE_VILLAGE.DESCRIPTION AS "DESCRIPTION_CATEGORIE_VILLAGE",
+	LEFT(FORMAT(AVG(TARIF_NUITEE.TARIF_UNITAIRE), 'C', 'fr-CA'), 10) AS "PRIX_PAR_NUIT_PAR_PERSONNE"
+FROM
+	TARIF_NUITEE
+	INNER JOIN CATEGORIE_VILLAGE
+		ON TARIF_NUITEE.ID_CATEGORIE_VILLAGE = CATEGORIE_VILLAGE.ID_CATEGORIE_VILLAGE
+GROUP BY
+	CATEGORIE_VILLAGE.NO_CATEGORIE,
+	CATEGORIE_VILLAGE.DESCRIPTION
+ORDER BY 
+	CATEGORIE_VILLAGE.NO_CATEGORIE
+
+/*
+NO_CATEGORIE DESCRIPTION_CATEGORIE_VILLAGE                      PRIX_PAR_NUIT_PAR_PERSONNE
+------------ -------------------------------------------------- --------------------------
+1            tennis, piscine, mini-golf, golf, sauna, garderie  57,27 $
+2            tennis, piscine, golf, sauna                       48,64 $
+3            tennis, piscine, garderie                          43,64 $
+
+(3 lignes affectées)
+*/
+
 
 
